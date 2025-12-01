0d02330f3968e117d732427db5de921a3345e85d
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-12-01 14:21:29 +0530

    Reusable class with generics

--- Android Basics - Unit 3/ReusableClassWithGenerics.kt
+++ Android Basics - Unit 3/ReusableClassWithGenerics.kt
@@ -1,22 +1,32 @@
-class FillInTheBlankQuestion(
+class Question<T> (
     val questionText: String,
-    val answer: String,
-    val difficulty: String
+    val answer: T, 
+    val diffculty: String
 )
 
-class TrueOrFalseQuestion(
-    val questionText: String,
-    val answer: Boolean,
-    val difficulty: String
-)
+// So this Question class can now be used to create questions of different types like String, Int, Boolean etc.
+// Why generics?
+// 1. Code Reusability: Ekbar class likhe onek type er jonno use kora jabe
+// 2. Type Safety: Compile time e type check hoye jabe automatically, jate kore runtime e type error na hoy
+// 3. Flexibility: Different type er data handle korte parbe ektai class diye
 
-class NumericQuestion(
-    val questionText: String,
-    val answer: Int,
-    val difficulty: String
-)
 
+// Example below:
 
 fun main () {
-
+    val question1 = Question<String>(
+        "What is the capital of France?",
+        "Paris",
+        "Easy"
+    )
+    val question2 = Question<Int>(
+        "What is 2 + 2?",
+        4,
+        "Easy"
+    )
+    val question3 = Question<Boolean>(
+        "Is the sky blue?",
+        true,
+        "Easy"
+    )
 }
