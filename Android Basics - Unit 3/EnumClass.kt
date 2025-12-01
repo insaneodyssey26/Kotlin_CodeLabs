757dc8a80982ef28a1e2371c71651e18d16b15b4
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-12-01 15:36:14 +0530

    Enum class

---
+++ Android Basics - Unit 3/EnumClass.kt
@@ -0,0 +1,35 @@
+class Question<T> (
+    val questionText: String,
+    val answer: T, 
+    val diffculty: Difficulty
+)
+
+// Why use an enum class?
+// 1. To represent a fixed set of constants
+// 2. To improve code readability and maintainability
+// 3. To group related constants together
+// 4. To provide type safety
+
+enum class Difficulty {
+    EASY,
+    MEDIUM,
+    HARD
+}
+
+fun main () {
+    val question1 = Question<String>(
+        "What is the capital of France?",
+        "Paris",
+        Difficulty.MEDIUM
+    )
+    val question2 = Question<Int>(
+        "What is 2 + 2?",
+        4,
+        Difficulty.EASY
+    )
+    val question3 = Question<Boolean>(
+        "Is the sky blue?",
+        true,
+        Difficulty.EASY
+    )
+}
